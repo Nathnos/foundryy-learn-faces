a1d4dde18047fa48505dfc9efa340159c0ed329f
Lorenzo VILLARD <villard.lorenzo.pro@pm.me>
2023-02-14 11:53:43 +0100

    add aws_handler.py empty for now

---
+++ aws_handler.py
@@ -0,0 +1,18 @@
+"""
+(file) : Handle AWS (Amazon Web Services)
+"""
+
+"""
+Retrieve a request from the SQS queue storing requests of face learning
+"""
+def get_sqs_request():
+    # TODO get an SQS request
+    request = []
+    return download_images(request)
+
+"""
+Download selfies of the user.
+"""
+def download_images(request):
+    # TODO get from S3 buckets
+        return True
