cd027db58f30f8f11ab0bf1ce1e165d80ea5d978
Lorenzo VILLARD <villard.lorenzo.pro@pm.me>
2023-02-15 15:29:39 +0100

    update setup-ec2.sh

--- setup-ec2.sh
+++ setup-ec2.sh
@@ -1,16 +1,14 @@
-sudo yum install git
 git clone https://github.com/Nathnos/foundryy-learn-faces
 mkdir ~/foundryy-learn-faces/selfies
 mkdir ~/foundryy-learn-faces/cropped_selfies
 cd ~/foundryy-learn-faces/selfies
 unzip selfies.zip
+rm selfies.zip
 
-cd ..
+cd foundryy-learn-faces
 pip3 install -r requirements.txt
 pip3 install git+https://github.com/huggingface/diffusers
-accelerate config
-python -c "from accelerate.utils import write_basic_config; write_basic_config(mixed_precision='fp16')"
 
 python3 preprocess.py
-chmod +x train.bat
-./train.bat
+chmod +x train.sh
+./train.sh
